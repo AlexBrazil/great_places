49cb8c7efa22706b2bbb87ca3bd7cdd047d6612e
AlexBrazil <alexandre@faciltecnologias.com.br>
2021-05-08 09:25:26 -0300

    Melhorando o formulário NOVO LUGAR

--- Great_Places/lib/Screens/place_form_screen.dart
+++ Great_Places/lib/Screens/place_form_screen.dart
@@ -1,3 +1,4 @@
+import 'package:Great_Places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 
 class PlaceFormScreen extends StatefulWidget {
@@ -6,14 +7,54 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _submitForm() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Novo Lugar'),
       ),
-      body: Center(
-        child: Text('Form!!!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // Isso faz o botão Adicionar ir para o fim da tela
+          Expanded(
+            // É para garantir que nada fique oculto se a tela for menor
+            // O Botão Adicionar fica fora do SingleChildScrollView, por isso
+            // estará sempre na tela
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    // Não vamos usar um Form, mas apenas um TextField
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            onPressed: _submitForm,
+            icon: Icon(Icons.add),
+            label: Text('Adicionar'),
+            color: Theme.of(context).accentColor,
+            elevation: 0,
+            // MaterialTapTargetSize.shrinkWrap - Reduz o tamanho do alvo de toque ao mínimo fornecido pela especificação do material
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          )
+        ],
       ),
     );
   }
