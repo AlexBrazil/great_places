563c2ac781af8d4ab123ae40cde0b751d205828a
AlexBrazil <alexandre@faciltecnologias.com.br>
2021-05-08 15:38:40 -0300

    Acessando a câmera e tirando foto

--- Great_Places/lib/widgets/image_input.dart
+++ Great_Places/lib/widgets/image_input.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -6,23 +8,46 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  // Faz a captura da câmera
+  _takePicture() async {
+    final ImagePicker _picker = ImagePicker();
+    PickedFile imageFile = await _picker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    // Se o usuário retornar sem tirar foto
+    if (imageFile == null) return;
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Container(
-          alignment: Alignment.center,
-          width: 180,
-          height: 100,
-          decoration: BoxDecoration(
-            border: Border.all(width: 1, color: Colors.grey),
-          ),
-          child: Text('Nenhuma Imagem!!'),
-        ),
+            alignment: Alignment.center,
+            width: 180,
+            height: 100,
+            decoration: BoxDecoration(
+              border: Border.all(width: 1, color: Colors.grey),
+            ),
+            child: _storedImage != null
+                ? Image.file(
+                    _storedImage,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  )
+                : Text('Nenhuma Imagem!!')),
         SizedBox(width: 10),
         Expanded(
           child: FlatButton.icon(
-            onPressed: () {},
+            onPressed: _takePicture,
             icon: Icon(Icons.camera),
             label: Text('Tirar Foto'),
             textColor: Theme.of(context).primaryColor,
