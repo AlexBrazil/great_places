9e03bc395519e837e956fc353cef34e3a842ef65
AlexBrazil <alexandre@faciltecnologias.com.br>
2021-05-13 06:30:27 -0300

    Criada a classe DBUtil para manipular o SQLite

--- Great_Places/lib/providers/great_places.dart
+++ Great_Places/lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:math';
 
+import 'package:Great_Places/utils/db_util.dart';
 import 'package:flutter/cupertino.dart';
 import '../models/place.dart';
 
@@ -27,6 +28,14 @@ class GreatPlaces with ChangeNotifier {
       location: null,
     );
     _items.add(newPlace);
+    DbUtil.insert(
+      'places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+      },
+    );
     notifyListeners();
   }
 }
---
+++ Great_Places/lib/utils/db_util.dart
@@ -0,0 +1,30 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DbUtil {
+  static Future<sql.Database> database() async {
+    // Armazena o caminho onde está o database
+    final dbPath = await sql.getDatabasesPath();
+    // Será criado um database no path com o nome places.db
+    return sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      // Este método é chamado apenas quando o banco é criado
+      onCreate: (db, version) {
+        // Aqui se usa o DDL - data definitio language
+        db.execute(
+            'CREATE TABLE places (id TEXT PRIMARY KEY, title TEXT, image TEXT)');
+      },
+      version: 1,
+    );
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DbUtil.database();
+    await db.insert(
+      table,
+      data,
+      // Caso exista alguma duplicidade será subistituído
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+  }
+}
